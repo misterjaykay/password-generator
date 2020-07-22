4b07e98f8e36513acca29f773e0ade67284e521f
Jonghyun Kang <misterjaykay@gmail.com>
2020-07-21 23:28:23 -0400

    begin writting js script

---
+++ pseudocode-password.md
@@ -0,0 +1,47 @@
+Conditions
+
+1. scipt asking how many charater do you want your password to be
+
+* if you are going to have your password least 8 characters long, it will proceed.
+
+* if you are going to have your password less then 8 characters long, it will abort.
+
+
+
+2. script will ask if the user want to add special characters to the password.
+
+* If yes, add the special characters to the password pool.
+
+* If not, do not add the special characters to the password pool.
+
+
+
+3. script will ask if the user want to add numeric values to the password.
+
+* If yes, add the numeric values to the password pool.
+
+* If not, do not add the numeric values to the password pool.
+
+
+
+4. script will ask if the user want to add lowercase characters to the password.
+
+* If yes, add the lowercase characters to the password pool.
+
+* If not, do not add the lowercase characters to the password pool.
+
+
+
+5. script will ask if the user want to add uppercase characters to the passwrd.
+
+* If yes, add the uppercase characters to the password pool.
+
+* If not, do not add the uppercase characters  to the password pool.
+
+
+
+6. script will generate password with what options user choose.
+
+7. script will display the password on index.html page
+
+
--- script.js
+++ script.js
@@ -10,5 +10,60 @@ function writePassword() {
 
 }
 
+// asking length of character size
+    var password = prompt("How long do you want your password to be? \nWarning:")
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Password arrays for example
+var lowerCase = ["a","b","c","d","e","f","g","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    for (var i = 0; i < lowerCase.length; i++) {
+        console.log(lowerCase[i]);
+    }
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    for (var j = 0; j < 10; j++) {
+
+        var testRun = upperCase[Math.floor(Math.random() * upperCase.length)];
+        console.log(testRun)
+    }
+
+
+var specialChar = ["!",",","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+
+    for (var h = 0; h < 10; h++) {
+
+        var testRun = specialChar[Math.floor(Math.random() * specialChar.length)];
+        console.log(testRun)
+    }
+
+var numericValue = ["1","2","3","4","5","6","7","8","9","0"];
+
+    for (var k = 0; k < 10; k++) {
+        
+        var testRun = numericValue[Math.floor(Math.random() * numericValue.length)];
+        console.log(testRun)
+    }
+
+
+// testing out some code and results // 
+// var test = "abcdefghijklmnopqrstuvwxyz"
+// var test1 = test.split("")
+// for (var j = 0; j < 10; j++) {
+
+//     var testRun = test1[Math.floor(Math.random() * upperCase.length)];
+//         console.log(testRun)
+//     }
+// VM537:4 p
+// VM537:4 j
+// VM537:4 h
+// VM537:4 u
+// VM537:4 k
+// VM537:4 r
+// 2VM537:4 g
+// VM537:4 p
+// VM537:4 m
