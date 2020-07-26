9203ddd8b1de9814fb3bc91323ee2a4ec19b5474
Jonghyun Kang <misterjaykay@gmail.com>
2020-07-26 01:18:45 -0400

    condensing javascript lines

--- script.js
+++ script.js
@@ -5,69 +5,59 @@ var passwordUpper = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
 var passwordSpec = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var passwordNumber = "1234567890";
 
-var passwordSpecArray = passwordSpec.split("");
-var passwordNumberArray = passwordNumber.split("");
-var passwordUpperArray = passwordUpper.split("");
-var passwordLowerArray = passwordLower.split("");
-
-
-
 function generatePassword() {
   var passwordPool = "";
   var poolPick ="";
   var passwordAsk = prompt("How long do you want your password to be?")
  
-    if (passwordAsk >= 8 && passwordAsk < 129) {
-      console.log(passwordAsk);  
+    if (passwordAsk >= 8 && passwordAsk < 129) { 
+
+      var passwordAskSpecial = confirm("Do you want to add special charcters to your password?")
+      if (passwordAskSpecial === true) {
+        passwordPool += passwordSpec;
+        for (var i = 0; i < 1; i++) {
+          var randomSpec = Math.floor(Math.random() * passwordSpec.length);
+          poolPick += passwordSpec[randomSpec]; 
+        }
+      }
+
+      var passwordAskUpper = confirm("Do you want to add upper case letters to your password?")
+      if (passwordAskUpper === true) {
+        passwordPool += passwordUpper;
+        for (var i = 0; i < 1; i++) {
+          var randomUpper = Math.floor(Math.random() * passwordUpper.length);
+          poolPick += passwordUpper[randomUpper]; 
+        }
+      }
+
+      var passwordAskNumber = confirm("Do you want to add numeric values to your password?")
+      if (passwordAskNumber === true) {
+        passwordPool += passwordNumber;
+        for (var i = 0; i < 1; i++) {
+          var randomNumb = Math.floor(Math.random() * passwordNumber.length);
+          poolPick += passwordNumber[randomNumb]; 
+        }
+      }
+
+      var passwordAskLower = confirm("Do you want to add lower case letters to your password?")
+      if (passwordAskLower === true) {
+        passwordPool += passwordLower;
+        for (var i = 0; i < 1; i++) {
+          var randomLower = Math.floor(Math.random() * passwordLower.length);
+          poolPick += passwordLower[randomLower]; 
+        }
+      }
+
+      if (passwordAskLower === false && 
+        passwordAskNumber === false && 
+        passwordAskSpecial === false && 
+        passwordAskUpper === false) {
+        alert("Password cannot be made. \nYou need to choose at least one character type."); 
+        return;
+      }
     }
     else {
       alert("You have to have more than 8 letters and less than 128 letters"); 
-      return;
-    }
-
-    var passwordAskSpecial = confirm("Do you want to add special charcters to your password?")
-    if (passwordAskSpecial === true) {
-      passwordPool += passwordSpec;
-      for (var i = 0; i < 1; i++) {
-        var randomSpec = Math.floor(Math.random() * passwordSpec.length);
-        poolPick += passwordSpec[randomSpec]; 
-      }
-    }
-
-    var passwordAskUpper = confirm("Do you want to add upper case letters to your password?")
-    if (passwordAskUpper === true) {
-      passwordPool += passwordUpper;
-      for (var i = 0; i < 1; i++) {
-        var randomUpper = Math.floor(Math.random() * passwordUpper.length);
-        poolPick += passwordUpper[randomUpper]; 
-      }
-    }
-
-    var passwordAskNumber = confirm("Do you want to add numeric values to your password?")
-    if (passwordAskNumber === true) {
-      passwordPool += passwordNumber;
-      for (var i = 0; i < 1; i++) {
-        var randomNumb = Math.floor(Math.random() * passwordNumber.length);
-        poolPick += passwordNumber[randomNumb]; 
-      }
-    }
-
-    var passwordAskLower = confirm("Do you want to add lower case letters to your password?")
-    if (passwordAskLower === true) {
-      passwordPool += passwordLower;
-      for (var i = 0; i < 1; i++) {
-        var randomLower = Math.floor(Math.random() * passwordLower.length);
-        poolPick += passwordLower[randomLower]; 
-      }
-    }
-    console.log(passwordPool);
-    console.log(poolPick);
-
-    if (passwordAskLower === false && 
-      passwordAskNumber === false && 
-      passwordAskSpecial === false && 
-      passwordAskUpper === false) {
-      alert("Password cannot be made. \nYou need to choose at least one character type."); 
       return;
     }
 
@@ -77,12 +67,10 @@ function generatePassword() {
     index = Math.floor(Math.random() * passwordPool.length);
     poolPickMore.push(passwordPool[index]);
   }
-  console.log(poolPickMore);
-  console.log(poolPick);
+
   var poolPickMoreFinal = poolPickMore.join("");
   var finalPassword = poolPick + poolPickMoreFinal;
 
-  console.log(finalPassword.length);
   console.log("Final password is ", finalPassword);
   return finalPassword;
 }
